1bd2d9683df5b89a2748ef505c5aec6cf10abca2
Ashton Pooley <ashton.p404@gmail.com>
2025-10-02 11:16:32 +0930

    Clean up package and fix import path

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name="smartsheet_tools",
-    version="0.0.1",
+    version="0.0.2",
     description="A collection of convenience functions to aid with transitioning from simple-smartsheet to the SDK API and common tasks",
     author="Ashton Pooley",
     author_email="Ashton@ashi.digital",
