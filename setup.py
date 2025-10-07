7d9418419505b9982e4a408744dfa11902f41ca4
Ashton Pooley <ashton.p404@gmail.com>
2025-10-07 22:40:09 +1030

    0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="smartsheet_tools",
-    version="0.0.5",
+    version="0.0.6",
     description="A collection of convenience functions to aid with transitioning from simple-smartsheet to the SDK API and common tasks",
     author="Ashton Pooley",
     author_email="Ashton@ashi.digital",
